d3b791186f69bdb9652ba19d377ad692c532e106
Enio <enio1303@gmail.com>
2022-03-23 11:44:24 -0300

    Criado crud de Companhia

---
+++ src/companhia/companhia.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { CompanhiaService } from './companhia.service';
+import { CompanhiaController } from './companhia.controller';
+import { Companhia } from '../models/CompanhiaEntity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Companhia])],
+  controllers: [CompanhiaController],
+  providers: [CompanhiaService],
+})
+export class CompanhiaModule {}
--- src/models/CompanhiaEntity.ts
+++ src/models/CompanhiaEntity.ts
@@ -1,23 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
-import { Funcionario } from "./FuncionarioEntity";
-import { Onibus } from "./OnibusEntity";
-import { Viagem } from "./ViagemEntity";
-
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Funcionario } from './FuncionarioEntity';
+import { Onibus } from './OnibusEntity';
+import { Viagem } from './ViagemEntity';
 
 @Entity()
 export class Companhia {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    nome: string;
+  @Column()
+  nome: string;
 
-    @OneToMany(() => Funcionario, (funcionario) => funcionario.companhia)
-    funcionarios: Funcionario[];
+  @OneToMany(() => Funcionario, (funcionario) => funcionario.companhia)
+  funcionarios: Funcionario[];
 
-    @OneToMany(() => Onibus, (onibus) => onibus.companhia)
-    onibus: Onibus[];
+  @OneToMany(() => Onibus, (onibus) => onibus.companhia)
+  onibus: Onibus[];
 
-    @OneToMany(() => Viagem, (viagem) => viagem.companhia)
-    viagens: Viagem[];
+  @OneToMany(() => Viagem, (viagem) => viagem.companhia)
+  viagens: Viagem[];
 }
