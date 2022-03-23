e024bbf3fde448f9aeae5e0aeac0e7fe0a51977b
Enio <enio1303@gmail.com>
2022-03-23 09:36:34 -0300

    Criado auth login jwt token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService  
+  ) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req): Promise<any>{
+    return this.authService.login(req.user);
+  }
 
   @Get()
   getHello(): string {
