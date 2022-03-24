5a111356fa17e1b43b5438246d80dab0064c230f
Enio <enio1303@gmail.com>
2022-03-24 10:09:58 -0300

    Adicionando o inicio do swagger nos controller funcionario e companhia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Rodoviaria Raro')
+    .setDescription('Api feita em Nestjs para o exercício da Raro Academy.')
+    .setVersion('1.0')
+    .addTag('rodoviariararo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
